364ee1a7bd92979de3d3da5dee50a10c03c719a6
saguirregaray1 <saguirregaray1@correo.um.edu.uy>
2024-06-06 12:45:44 -0300

    add user model

--- bustimeapp/apps.py
+++ bustimeapp/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class BustimeappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'bustimeapp'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "bustimeapp"
--- bustimeapp/views.py
+++ bustimeapp/views.py
@@ -1,30 +1,34 @@
+from rest_framework import status, viewsets, generics, authentication, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework import status
-import requests
-from django.conf import settings
-from rest_framework import viewsets
-from .models import Stop
-from .serializers import StopSerializer
-from rest_framework.decorators import action
-from drf_spectacular.utils import extend_schema
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
+
+import requests
+
+from django.conf import settings
+from .models import Stop
+from .serializers import StopSerializer, UserSerializer, AuthTokenSerializer
+from drf_spectacular.utils import extend_schema
 
 
 def get_token():
 
     headersList = {
-    "Accept": "*/*",
-    "User-Agent": "*",
-    "Content-Type": "application/x-www-form-urlencoded" 
+        "Accept": "*/*",
+        "User-Agent": "*",
+        "Content-Type": "application/x-www-form-urlencoded",
     }
 
     payload = f"grant_type=client_credentials&client_id={settings.CLIENT_ID}&client_secret={settings.CLIENT_SECRET}"
     token = None
 
     try:
-        response = requests.request("POST", settings.TOKEN_URL, data=payload,  headers=headersList)
-        token = response.json()['access_token']
+        response = requests.request(
+            "POST", settings.TOKEN_URL, data=payload, headers=headersList
+        )
+        token = response.json()["access_token"]
     except Exception:
         pass
 
@@ -36,16 +40,18 @@ def get_request(token, api_url, endpoint, params=None):
     if params is not None:
         req_params = params
 
-    response = requests.get(api_url + endpoint, headers = {
-            'Authorization': f'Bearer {token}',
+    response = requests.get(
+        api_url + endpoint,
+        headers={
+            "Authorization": f"Bearer {token}",
             "Accept": "/",
             "User-Agent": "*",
-            "Content-Type": "application/x-www-form-urlencoded"
-        },params=req_params)
-    
+            "Content-Type": "application/x-www-form-urlencoded",
+        },
+        params=req_params,
+    )
+
     return response
-
-
 
 
 class GetBusesView(APIView):
@@ -56,13 +62,13 @@ class GetBusesView(APIView):
             response = get_request(token, settings.API_URL, settings.BUSES_ENDPOINT)
             if response.status_code < 300:
                 buses = response.json()
-            
+
         except Exception as e:
             print(e)
             pass
-        
+
         return Response(buses)
-    
+
 
 class GetStopsView(APIView):
     def get(self, request):
@@ -75,7 +81,7 @@ class GetStopsView(APIView):
         if response.status_code < 300:
             stops = response.json()
 
-        # try:    
+        # try:
         #     response = get_request(token, settings.API_URL, settings.STOPS_ENDPOINT)
 
         #     if response.status_code < 300:
@@ -89,12 +95,14 @@ class GetStopsView(APIView):
 
 class GetStopInfoView(APIView):
     def get(self, request, *args, **kwargs):
-        stop_id = self.kwargs['stop_id']
+        stop_id = self.kwargs["stop_id"]
         token = get_token()
         stop_info = None
-    
-        try:    
-            response = get_request(token, settings.API_URL, f'{settings.STOPS_ENDPOINT}/{stop_id}')
+
+        try:
+            response = get_request(
+                token, settings.API_URL, f"{settings.STOPS_ENDPOINT}/{stop_id}"
+            )
 
             if response.status_code < 300:
                 stop_info = response.json()
@@ -102,13 +110,13 @@ class GetStopInfoView(APIView):
             print(e)
 
         return Response(stop_info)
-    
-    
+
+
 class StopViewSet(viewsets.ViewSet):
 
     def get_queryset(self):
-            return Stop.objects.all()  
-          
+        return Stop.objects.all()
+
     @extend_schema(responses=StopSerializer)
     def list(self, request):
         """
@@ -119,16 +127,42 @@ class StopViewSet(viewsets.ViewSet):
         serializer = StopSerializer(queryset, many=True)
         return Response(serializer.data)
 
-    @extend_schema(request=StopSerializer(many=True), responses=StopSerializer(many=True))
+    @extend_schema(
+        request=StopSerializer(many=True), responses=StopSerializer(many=True)
+    )
     def create(self, request):
         """
         Endpoint to create a new stop
         """
         serializer = StopSerializer(data=request.data, many=True)
         if serializer.is_valid():
-            serializer.save()                 
+            serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        
+
         print(serializer.errors)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        
+
+
+class CreateUserView(generics.CreateAPIView):
+    """Create a new user in the system"""
+
+    serializer_class = UserSerializer
+
+
+class CreateTokenView(ObtainAuthToken):
+    """Create a new auth token for user"""
+
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage the authenticated user."""
+
+    serializer_class = UserSerializer
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        """Retrieve and return the authenticated userr"""
+        return self.request.user
