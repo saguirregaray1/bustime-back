f69ff5a72cc5c7c3f82a780e1dae036f01e76843
saguirregaray1 <saguirregaray1@correo.um.edu.uy>
2024-06-16 19:31:41 -0300

    fix disconnect

--- bustimeapp/consumers.py
+++ bustimeapp/consumers.py
@@ -5,35 +5,36 @@ from .utils import get_token, get_request, get_upcoming_buses
 from django.conf import settings
 import asyncio
 
+
 class LiveBustopConsumer(AsyncWebsocketConsumer):
+    def __init__(self):
+        self.is_connected = True
+        self.groups = []
+
     async def connect(self):
         await self.accept()
-        
-        await self.send(text_data=json.dumps({
-            'message': 'Connected'
-        }))
-        
+
+        await self.send(text_data=json.dumps({"message": "Connected"}))
 
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
 
-        stop_id = text_data_json['stop_id']
+        stop_id = text_data_json["stop_id"]
         if stop_id is not None:
             await self.send_updates(stop_id)
-        
-        
+
     async def disconnect(self, close_code):
-        pass
-        
-    async def send_updates(self, stop_id):    
+        self.is_connected = False
+
+    async def send_updates(self, stop_id):
         token = await get_token()
-        url=f"{settings.STOPS_ENDPOINT}/{stop_id}"
+        url = f"{settings.STOPS_ENDPOINT}/{stop_id}"
         first = True
-        while True:  
+        while self.is_connected:
             stop_info = None
             try:
                 if first:
-                    first=False
+                    first = False
                 else:
                     await asyncio.sleep(8)
                 response = await get_request(token, settings.API_URL, url)
@@ -43,7 +44,9 @@ class LiveBustopConsumer(AsyncWebsocketConsumer):
                     if response.status_code == 429:
                         await asyncio.sleep(20)
                     else:
-                        raise Exception(f"Request failed with status {response.status_code}. Message: {error_message}")
+                        raise Exception(
+                            f"Request failed with status {response.status_code}. Message: {error_message}"
+                        )
 
                 stop_info = response.json()
 
@@ -51,14 +54,9 @@ class LiveBustopConsumer(AsyncWebsocketConsumer):
                 upcoming_buses = await get_upcoming_buses(token, stop_id, bus_lines)
                 if upcoming_buses is not None:
                     stop_info["upcoming_buses"] = upcoming_buses
-                text_data=json.dumps(stop_info)
+                text_data = json.dumps(stop_info)
                 await self.send(text_data=text_data)
             except Exception as e:
-                await self.send(text_data=json.dumps({
-                    'error': str(e)
-                })
-                )
-                break
-
-            
-    
+                await self.send(text_data=json.dumps({"error": str(e)}))
+                self.is_connected = False
+                await self.close()
